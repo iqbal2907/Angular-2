fde0a764202dcd62b8ae6d709c24c0408388422b
Ashrafi Iqbal <ashrafii@opentext.com>
2018-01-31 17:15:02 +0530

    renamed for 6,7 to 06,07 also removed app component from tutorial 14 folder and kept in app component itself in the comment

--- app/app.component.js
+++ app/app.component.js
@@ -9,19 +9,20 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
-//import { TutorialsComponent } from './concepts/Tutorial_6_Creating_your_first_component/tutorials.component';
-//import { TutorialsComponent } from './concepts/Tutorial_7_Styling_Components/tutorials.component';
-//import { TutorialsComponent } from './concepts/Tutorial _10 _Event_Binding_and_References/tutorials.component';
-//import { TutorialsComponent } from './concepts/Tutorial _11 _Two_Way_Binding/tutorials.component';
+//import { TutorialsComponent } from './concepts/Tutorial_06_Creating_your_first_component/tutorials.component';
+//import { TutorialsComponent } from './concepts/Tutorial_07_Styling_Components/tutorials.component';
+//import { TutorialsComponent } from './concepts/Tutorial_10_Event_Binding_and_References/tutorials.component';
+//import { TutorialsComponent } from './concepts/Tutorial_11_Two_Way_Binding/tutorials.component';
 //import { TutorialsComponent } from './concepts/Tutorial_12_Structural_Directives/tutorials.component';
-var tutorials_component_1 = require('./concepts/Tutorial_13_Attribute_Directives/tutorials.component');
+//import { TutorialsComponent } from './concepts/Tutorial_13_Attribute_Directives/tutorials.component';
+var tutorials_component_1 = require('./concepts/Tutorial_14_Inputs_and_Outputs/tutorials.component');
 var AppComponent = (function () {
     function AppComponent() {
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "<h1>First Angular 2 App</h1>\n              <my-tutorials></my-tutorials>",
+            template: "<h1>First Angular 2 App</h1>\n              <h2>Parent App Component</h2>\n              <label>Enter Parent AppComponent value</label>\n              <input type=\"text\" #ptext (keyup)=\"0\">\n              <p>Value from child TutorialsComponent is : \n              {{childData}}\n              <my-tutorials (childEvent)=\"childData=$event\"[parentData]=\"ptext.value\"></my-tutorials>",
             directives: [tutorials_component_1.TutorialsComponent]
         }), 
         __metadata('design:paramtypes', [])
@@ -29,4 +30,16 @@ var AppComponent = (function () {
     return AppComponent;
 }());
 exports.AppComponent = AppComponent;
+/*
+//use below code for app.component.ts for all tutorial component on and before 13
+@Component({
+  selector: 'my-app',
+  template: `<h1>First Angular 2 App</h1>
+              <my-tutorials></my-tutorials>`,
+  directives: [TutorialsComponent]
+})
+export class AppComponent { }
+
+
+*/
 //# sourceMappingURL=app.component.js.map
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
-//import { TutorialsComponent } from './concepts/Tutorial_6_Creating_your_first_component/tutorials.component';
-//import { TutorialsComponent } from './concepts/Tutorial_7_Styling_Components/tutorials.component';
-//import { TutorialsComponent } from './concepts/Tutorial _10 _Event_Binding_and_References/tutorials.component';
-//import { TutorialsComponent } from './concepts/Tutorial _11 _Two_Way_Binding/tutorials.component';
+//import { TutorialsComponent } from './concepts/Tutorial_06_Creating_your_first_component/tutorials.component';
+//import { TutorialsComponent } from './concepts/Tutorial_07_Styling_Components/tutorials.component';
+//import { TutorialsComponent } from './concepts/Tutorial_10_Event_Binding_and_References/tutorials.component';
+//import { TutorialsComponent } from './concepts/Tutorial_11_Two_Way_Binding/tutorials.component';
 //import { TutorialsComponent } from './concepts/Tutorial_12_Structural_Directives/tutorials.component';
-import { TutorialsComponent } from './concepts/Tutorial_13_Attribute_Directives/tutorials.component';
+//import { TutorialsComponent } from './concepts/Tutorial_13_Attribute_Directives/tutorials.component';
+import { TutorialsComponent } from './concepts/Tutorial_14_Inputs_and_Outputs/tutorials.component';
+
+@Component({
+  selector: 'my-app',
+  template: `<h1>First Angular 2 App</h1>
+              <h2>Parent App Component</h2>
+              <label>Enter Parent AppComponent value</label>
+              <input type="text" #ptext (keyup)="0">
+              <p>Value from child TutorialsComponent is : 
+              {{childData}}
+              <my-tutorials (childEvent)="childData=$event"[parentData]="ptext.value"></my-tutorials>`,
+  directives: [TutorialsComponent]
+})
+export class AppComponent { 
+  public childData: string;
+}
 
 
+/*
+//use below code for app.component.ts for all tutorial component on and before 13
 @Component({
   selector: 'my-app',
   template: `<h1>First Angular 2 App</h1>
@@ -14,3 +32,9 @@ import { TutorialsComponent } from './concepts/Tutorial_13_Attribute_Directives/
   directives: [TutorialsComponent]
 })
 export class AppComponent { }
+
+
+*/
+
+
+
